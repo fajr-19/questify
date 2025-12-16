b0270a06043efb0b0a92dff4fc5ecec604aad827
fajr-19 <kontoru676@gmail.com>
2025-12-16 15:02:09 +0700

    Update backend server, fix MongoDB connection issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error('Error: MONGO_URI is not defined!');
+    process.exit(1); // Stop the server
+  }
+
   await mongoose.connect(process.env.MONGO_URI);
   console.log('MongoDB Atlas connected');
 };
