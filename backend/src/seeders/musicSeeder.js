d564eb4caf7824663256ab881402221d5d9a33cb
fajr-19 <kontoru676@gmail.com>
2025-12-28 19:40:53 +0700

    update seeders

--- backend/src/seeders/musicSeeder.js
+++ backend/src/seeders/musicSeeder.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../../.env' }); // Pastikan path env benar
+require('dotenv').config();
 const mongoose = require('mongoose');
 const axios = require('axios');
 const Music = require('../models/music');
